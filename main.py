f509bb957cc18de148715e173b1cddd46214b0d3
SHENG-WEI TSAI <henry88002605@gmail.com>
2026-03-07 09:29:54 +1000

    make a function calling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from call_function import available_functions
 
 
 def main():
@@ -23,6 +24,7 @@ def main():
         model="gemini-2.5-flash",
         contents=user_prompt,
         config=types.GenerateContentConfig(
+            tools=[available_functions],
             system_instruction=system_prompt,
             temperature=0,
         ),
@@ -33,8 +35,13 @@ def main():
 
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name} with arguments {function_call.args}")
+    else:
+        print("Response:")
+        print(response.text)
 
 
 if __name__ == "__main__":
